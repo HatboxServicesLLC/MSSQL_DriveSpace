f774ac379dba1c744792b31d45ea141fd8acf22e
CPearson <ChrisPearsonJax@live.com>
2024-05-28 13:20:57 -0400

    Add check to see if running on Linux or Windows OS.

--- source/Stored Procedures/dba.DriveSpace_Get_AverageOnDayForLastWeeks.sql
+++ source/Stored Procedures/dba.DriveSpace_Get_AverageOnDayForLastWeeks.sql
@@ -3,78 +3,79 @@ GO
 SET ANSI_NULLS ON
 GO
 /*****************************************************************************************************************************************************
-Name: dba.DriveSpace_Get_AverageOnDayForLastWeeks
-Creation Date: 03.12.2024
+Name: dba.DriveSpace_Get_Data
+Creation Date: 05.01.2024
 Author: CPearson
 
-Description:Get the average rate of use for given drives, over on given date, and the prior x weeks for that day. 
-
-
-Version Naming Conventions
---------------------------------------
-1.2
-^ ^
-| |
-| +----- Version Minor: features, major bug fixes, etc.
-+------- Version Major: syntax changes, join logic, table adds / removal changes, etc.
-
-Revision History:
------------------------
-Revision Date | Revision Developer | Version Major | Version Minor  | Ticket 			| Revision Notes
-------------------------------------------------------------------------------------------------------------------------
-03.12.2024 CPearson        1                      0     			DO-2713		Initial stored procedure
-
-Test Case:
--------------
-- **this section should include detailed instructions of how to test the intended functionality of this stored procedure**
+Description:Retrive the drive space information 
 
 ******************************************************************************************************************************************************/
-CREATE   procedure [dba].[DriveSpace_Get_AverageOnDayForLastWeeks] 
-(
-/*---------------------------------------
---External Variable Declaration
----------------------------------------*/
-@Date DATETIME,
-@NumWeeksBack TINYINT,
-@DriveLetters nvarchar(125) NULL
-/*---------------------------------------
----------------------------------------*/
-)
+CREATE or alter  procedure [dba].[DriveSpace_Get_Data] 
 AS
 BEGIN
-	BEGIN TRY	
-	/*validation*/
-	--DriveLetters is comma delimited list
-	/*--------------------------------------
-	--Internal Variable Declaration
-	--------------------------------------*/
-	 
-	
-	-- << begin code logic >> --
-		/* Averaged over Period*/
-		SELECT v.Drive,
-		       v.Drive_Letter,
-		       --v.DriveTotalSpace_GB,
-			   --CONVERT(DATE,v.CapturedDateTime) AS CapturedDate,
-		       SUM(v.Change_GBaSecond) / COUNT(*) AS Sum_Change_GBaSecond,
-		       SUM(v.Change_GBaMinute) / COUNT(*) AS Change_GBaMinute,
-		       SUM(v.Change_GBaHour) / COUNT(*) AS Change_GBaHour
-		FROM dba.vw_DriveSpace_Rates v
-		WHERE (DATEPART(day,v.CapturedDateTime) = DATEPART(DAY,@Date)
-				AND v.CapturedDateTime BETWEEN DATEADD(WEEK,-@NumWeeksBack,@Date) AND @Date)
-			AND (@DriveLetters IS NULL 
-					OR v.Drive_letter IN (SELECT value FROM STRING_SPLIT(@DriveLetters,',') drives))
-		GROUP BY v.Drive,
-		         v.Drive_Letter
-		         --v.DriveTotalSpace_GB,
-				 --,CONVERT(DATE,v.CapturedDateTime)
-				 ;
-	-- << end code logic >> --
-	END TRY
-	BEGIN CATCH
 	     DECLARE
 	          @ErrorSeverity INT
 	          ,@ErrorMessage VARCHAR(MAX) ;
+	begin try
+	declare @HostInfo table 
+	(
+		OS nvarchar(256),
+		Edition_Distribution nvarchar(256),
+		Release nvarchar(256),
+		Service_Pack_Level nvarchar(256),
+		sku int,
+		OS_Language_Version int,
+		Architecture nvarchar(256)
+		);
+
+
+		
+	insert into @HostInfo
+	exec ('select * from sys.dm_os_host_info'); 
+
+	end try
+	begin catch
+		if ERROR_NUMBER() = 208
+		begin
+			/*DM does not exist, that means we are on version <2017 which only supported windows*/
+			insert into @HostInfo (OS)
+			values('Windows'); 
+		end else
+		begin
+			/*Unknown error*/
+	     SELECT
+	          @ErrorSeverity = error_severity()
+	          ,@ErrorMessage = object_schema_name(@@procid)+'.'+object_name(@@procid) +' '+ 'Errored:' +' '+ error_message()
+	
+	     RAISERROR ( @ErrorMessage, @ErrorSeverity, 1 )
+		end
+
+	end catch
+	BEGIN TRY	
+SET XACT_ABORT ON;  
+	
+	select distinct
+	@@SERVERNAME as ServerName,
+   HostInfo.OS,
+	DB_NAME(mf.database_id) AS DatabaseName,
+	mf.[type_desc] as FileType,mf.physical_name,
+	vs.*,
+	case HostInfo.OS
+		when 'Windows'
+			then ISNULL(
+						vs.volume_mount_point,
+						LEFT(mf.physical_name,CHARINDEX('\', mf.physical_name,1))
+						)
+		when 'Linux'
+			then LEFT(mf.physical_name,CHARINDEX('/', mf.physical_name,1))
+		end
+	from [master].sys.master_files AS mf
+	cross apply (select * from @HostInfo) HostInfo
+	cross apply [master].sys.dm_os_volume_stats(mf.database_id, mf.file_id) vs;
+
+	exec sp_spaceused @oneresultset = 1
+	END TRY
+	BEGIN CATCH
 	     SELECT
 	          @ErrorSeverity = error_severity()
 	          ,@ErrorMessage = object_schema_name(@@procid)+'.'+object_name(@@procid) +' '+ 'Errored:' +' '+ error_message()
